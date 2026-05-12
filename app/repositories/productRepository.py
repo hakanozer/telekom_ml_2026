dd13e44f7ad93c3fd61e64863ff8a79d04906294
Hakan ÖZER <hakanozer02@gmail.com>
2026-05-12 15:25:52 +0300

    Json

--- app/repositories/productRepository.py
+++ app/repositories/productRepository.py
@@ -11,3 +11,23 @@ def create_product(db: Session, product: productSchema.ProductCreate):
     db.commit()
     db.refresh(db_product)
     return db_product
+
+# productleri listelemek için bir fonksiyon oluşturuyoruz
+def get_products(db: Session):
+    return db.query(models.Product).all()
+
+# id değerinine göre product getirmek için bir fonksiyon oluşturuyoruz
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+# product save all işlemi için bir fonksiyon oluşturuyoruz
+def save_all(db: Session, products: list[productSchema.ProductCreate]):
+    db_products = []
+    for product in products:
+        db_product = models.Product(title=product.title, description=product.description, price=product.price)
+        db.add(db_product)
+        db_products.append(db_product)
+    db.commit()
+    for db_product in db_products:
+        db.refresh(db_product)
+    return db_products
--- app/routes/productRoute.py
+++ app/routes/productRoute.py
@@ -1,5 +1,5 @@
 # product route dosyasını oluşturuyoruz
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from ..repositories import productRepository
 from ..schemas import productSchema
 from ..db import connection
@@ -13,3 +13,30 @@ def create_product(product: productSchema.ProductCreate):
     db_product = productRepository.create_product(db, product)
     db.close()
     return db_product
+
+# productleri listeleme endpointi oluşturuyoruz
+@router.get("/list")
+def get_products():
+    db = connection.SessionLocal()
+    products = productRepository.get_products(db)
+    db.close()
+    return products
+
+# http://localhost:8000/products/1 gibi bir endpoint oluşturuyoruz
+@router.get("/{product_id}")
+def get_product(product_id: int):
+    db = connection.SessionLocal()
+    product = productRepository.get_product(db, product_id)
+    db.close()
+    # product bulunamazsa 404 hatası döndürüyoruz
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+# savea all endpointi oluşturuyoruz
+@router.post("/saveall")
+def save_all(products: list[productSchema.ProductCreate]):
+    db = connection.SessionLocal()
+    db_products = productRepository.save_all(db, products)
+    db.close()
+    return db_products
