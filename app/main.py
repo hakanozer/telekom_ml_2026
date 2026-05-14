b67523c9f970212c6599273c07f3ccf4927592a6
Hakan ÖZER <hakanozer02@gmail.com>
2026-05-14 16:07:24 +0300

    Days_4

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import userRoute, productRoute, mlRoute
 
 # database işlemleri için connection.py dosyasını import ediyoruz
 from .db import connection
 
-
 # Veritabanı tablolarını oluşturuyoruz
 connection.Base.metadata.create_all(bind=connection.engine)
 
 app = FastAPI()
+
+# add cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Tüm originlere izin ver
+    allow_methods=["*"],  # Tüm HTTP metodlarına izin ver
+    allow_headers=["*"],  # Tüm header'lara izin ver
+)
 
 app.include_router(userRoute.router, prefix="/users") 
 app.include_router(productRoute.router, prefix="/products")
